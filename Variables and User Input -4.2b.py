63ede147b168f79101bc2830039fe812db492c23
Fupeng Zhao <72841750+ConnerZhao929@users.noreply.github.com>
2020-11-15 21:59:17 -0800

    Setup

---
+++ Variables and User Input -4.2b.py
@@ -0,0 +1,6 @@
+# Conner Zhao 11/05/20
+name = input('Please enter your name')
+print('Welcome ' + name + ".")
+age = int(input("How old are you? "))
+age = age + 10
+print(name + ", you will be " + str(age) + " years old in 10 years.")
