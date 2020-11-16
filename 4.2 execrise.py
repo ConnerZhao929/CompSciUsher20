bb1c7309be0850bded8ae9030c5d3c8b0c0a0558
Fupeng Zhao <72841750+ConnerZhao929@users.noreply.github.com>
2020-11-15 21:52:55 -0800

    4.2 execrise

---
+++ 4.2 execrise.py
@@ -0,0 +1,7 @@
+#My name is Conner Zhao today's date is Nov 15th, 2020
+#This is my first real program!
+"""
+Python is a super cool and fun programming language!
+"""
+print('Hello world this command starts a new line \n Hello')
+print('This is a \t tab')
