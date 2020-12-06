da76f22231c6b762039b7a3d8892a596704b4c7f
Fupeng Zhao <72841750+ConnerZhao929@users.noreply.github.com>
2020-12-06 14:49:36 -0800

    lol

--- Exercises/4.4 Exercise A - sum.py
+++ Exercises/4.4 Exercise A - sum.py
@@ -1,4 +1,4 @@
-#Conner Zhao nov18th
+# Conner Zhao nov18th
 """
 Create a variable and assign it the value of 0
 Prompt the user for how many numbers they have
@@ -8,10 +8,10 @@ With a for loop, set it to repeat enough times to get all their values
 Output to the user, the sum of all values
 """
 sumNum = 0
-amntNum = input("How many numbers do you want to add together?: ")
-amntNum = int(amntNum)
-for i in range(amntNum):
-        numadded = input("Give me one of those numbers: ")
-        numadded = int(numadded)
-        sumNum = sumNum + numadded
+AmountNumber = input("How many numbers do you want to add together?: ")
+AmountNumber = int(AmountNumber)
+for i in range(AmountNumber):
+    NumAdded = input("Give me one of those numbers: ")
+    NumAdded = int(NumAdded)
+    sumNum = sumNum + NumAdded
 print("The sum of your numbers is : " + str(sumNum))
---
+++ Exercises/Lists - 4.5 Exercise.py
@@ -0,0 +1,13 @@
+import random
+#Conner Zhao 12/05/20
+"""
+Create a new program in Repl.it called "Lists." Your goal for this program is to create sections of code that will perform different outputs with the list called, names which contains the following values - Peter, Bruce, Steve, Tony, Natasha, Clint, Wanda, Hope, Danny, Carol and the list called numbers which contains the following values - 100, 50, 10, 1, 2, 7, 11, 17, 53, -8, -4, -9, -72, -64, -80. At the beginning of your program, add in any comments and create the two lists. Then create the following:
+
+ A loop that will output every other name in the names list.
+A loop that will output only the positive numbers in the numbers list.
+A loop that will output the sum of all the values in the numbers list.
+A loop that will output only the numbers that are odd.
+A loop that will output only the names that come before "Thor" in the alphabet from the names list.
+A loop that will  find the maximum or minimum value in the numbers list. This algorithm requires an additional variable that is assigned to the first element in the list. Then, in a loop compare each element to the variable. If the element is > (for max) or < (for min), assign the variable to the element. After the loop, print the variable.
+HINT: len() is a helpful method that can be used on strings or lists to determine how "long" they are. For lists, that would mean how many elements are inside and for strings, that would mean how many characters long it is.
+"""
