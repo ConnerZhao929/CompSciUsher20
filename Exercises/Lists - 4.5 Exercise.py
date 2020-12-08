f4755ff042786425190edec8f29f311d8c03fd78
Fupeng Zhao <72841750+ConnerZhao929@users.noreply.github.com>
2020-12-07 23:08:29 -0800

    lol

--- Exercises/Lists - 4.5 Exercise.py
+++ Exercises/Lists - 4.5 Exercise.py
@@ -1,5 +1,8 @@
 #Conner Zhao 12/05/20
 """
+The reason why using a list reduces complexity is because instead of using many variable to define each name, you can do it by simpkly by adding all the names into a list. If every name and number are defined in individual variables, it would be a lot more complex for the programmer because when they are printing something they'll have do prin() for every single one of the variables.
+"""
+"""
 Create a new program in Repl.it called "Lists." Your goal for this program is to create sections of code that will perform different outputs with the list called, names which contains the following values - Peter, Bruce, Steve, Tony, Natasha, Clint, Wanda, Hope, Danny, Carol and the list called numbers which contains the following values - 100, 50, 10, 1, 2, 7, 11, 17, 53, -8, -4, -9, -72, -64, -80. At the beginning of your program, add in any comments and create the two lists. Then create the following:
 
 A loop that will output every other name in the names list.
